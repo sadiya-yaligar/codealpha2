3d405fc2cbea936b51dfbcef614544e21e5447b3
sadiya-yaligar <sadiyayaligar8@gmail.com>
2025-04-15 17:35:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function appendValue(value) {
+    document.getElementById("result").value += value;
+  }
+  
+  function clearResult() {
+    document.getElementById("result").value = "";
+  }
+  
+  function calculateResult() {
+    try {
+      document.getElementById("result").value = eval(document.getElementById("result").value);
+    } catch (error) {
+      alert("Invalid Expression");
+    }
+  }
+  
